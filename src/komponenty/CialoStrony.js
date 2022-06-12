00e5c5a3011649a21923fd5412d2e8fa2ac202dd
rgryc <r.grycner@wp.pl>
2022-06-12 13:50:11 +0200

    first commit

---
+++ src/komponenty/CialoStrony.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './CialoStrony.css'
+
+
+function CialoStrony(){
+    return (
+        <div className={classes.Elementglowny}>
+
+    </div>
+    );
+
+}
+export  default CialoStrony
---
+++ src/komponenty/Naglowek.js
@@ -0,0 +1,19 @@
+import classes from "../App.module.css";
+import React from "react";
+
+function Naglowek() {
+    return (<div className={classes.NaglowekStrony}>
+            <div className={classes.LewaStrony}>
+                <div className={classes.Logos}>
+                    <div className={classes.SingleLogo}>Logo</div>
+                    <div className={classes.SingleLogo}>Logo2</div>
+                    <div className={classes.SingleLogo}>Logo3</div>
+                </div>
+                <div className={classes.Tytul}> Tytuł strony</div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Naglowek;
